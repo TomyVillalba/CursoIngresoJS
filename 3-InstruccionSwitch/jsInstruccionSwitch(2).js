e9254858afdb5d80b5f8489e9eb9daae677ff969
TomyVillalba <tomasrv18@gmail.com>
2018-07-10 12:27:55 -0300

    Terminar 2 y 3 de Switch (Y si es posible terminar todo Switch)

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -5,6 +5,33 @@ var mesDelAño = document.getElementById('mes').value;
 
 alert (mesDelAño);
 
+switch(mesDelAño)
+{
+    case enero: 
+
+    case febrero: 
+
+    case marzo:
+
+    case abril:
+
+    case mayo:  alert("Falta para el invierno");
+    break;
+    case junio:
+
+    case julio: 
+
+    case agosto: alert("Abrigate que hace frio");
+    break;
+    case septiempre:
+
+    case octubre: 
+           
+    case noviembre: 
+
+    case diciembre: alert("Ya pasamos el frio, ahora el calor!");
+    break;
+}
 
 
 
--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -3,7 +3,13 @@ function mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
-//alert (mesDelAño);
+    switch(mesDelAño)
+    {
+        case febrero: alert
+    }
+
+
+
 	
 	
 
